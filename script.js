13da23decfd6138d556af485ea3e21f71e6460da
Your Name <as582shubham@gmail.com>
2024-12-10 16:58:42 +0530

    `Removed counter design functionality and updated CSS styles`

--- script.js
+++ script.js
@@ -17,24 +17,24 @@ navBar.forEach(function(a){
   })
 })
 
-// Counter Design  
-document.addEventListener("DOMContentLoaded", () => {
-  function counter(id, start, end, duration) {
-    let obj = document.getElementById(id),
-      current = start,
-      range = end - start,
-      increment = end > start ? 1 : -1,
-      step = Math.abs(Math.floor(duration / range)),
-      timer = setInterval(() => {
-        current += increment;
-        obj.textContent = current;
-        if (current == end) {
-          clearInterval(timer);
-        }
-      }, step);
-  }
-  counter("count1", 0, 1287, 3000);
-  counter("count2", 100, 4500, 2500);
-  counter("count3", 0, 2860, 3000);
-  counter("count4", 0, 3942, 3000);
-});
+// // Counter Design  
+// document.addEventListener("DOMContentLoaded", () => {
+//   function counter(id, start, end, duration) {
+//     let obj = document.getElementById(id),
+//       current = start,
+//       range = end - start,
+//       increment = end > start ? 1 : -1,
+//       step = Math.abs(Math.floor(duration / range)),
+//       timer = setInterval(() => {
+//         current += increment;
+//         obj.textContent = current;
+//         if (current == end) {
+//           clearInterval(timer);
+//         }
+//       }, step);
+//   }
+//   counter("count1", 0, 1287, 3000);
+//   counter("count2", 100, 4500, 2500);
+//   counter("count3", 0, 2860, 3000);
+//   counter("count4", 0, 3942, 3000);
+// });
